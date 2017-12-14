9e0e7b858789cb26d2413b4da457b1cddf4ae1ec
Long Nguyen <nguyen_van_long@humancrest.co.jp>
2017-12-15 03:38:56 +0700

    Update setAllowedOrigins

--- websocket-react/src/App.js
+++ websocket-react/src/App.js
@@ -4,18 +4,25 @@ import './App.css';
 // import EmployeeList from './EmployeeList'
 import SockJS from 'sockjs-client'
 import Stomp from 'stompjs'
-
 class App extends Component {
 
-    componentDidMount() {
+    constructor(props) {
+        super(props)
+        let socket = new SockJS("/gs-guide-websocket");
+        this.stompClient = Stomp.over(socket);
+        
+    }
+
+
+
+    handleClick() {
         // Create stomp client over sockJS protocol (see Note 1)
-        let socket = new SockJS("/hello");
-        let stompClient = Stomp.over(socket);
+
 
         // callback function to be called when stomp client is connected to server (see Note 2)
         let connectCallback = function () {
             alert("connected!");
-            stompClient.subscribe('/topic/greetings', function (greeting) {
+            this.stompClient.subscribe('/topic/greetings', function (greeting) {
                 alert(JSON.parse(greeting.body).content);
             });
         };
@@ -27,13 +34,24 @@ class App extends Component {
         };
 
         // Connect as guest (Note 4)
-        stompClient.connect("guest", "guest", connectCallback, errorCallback);
+        this.stompClient.connect("guest", "guest", connectCallback, errorCallback);
+
+        // function showGreeting(message) {
+        //     alert(message);
+        // }
+
+        // // Connect as guest (Note 4)
+        // this.stompClient.connect({}, function (frame) {
+        //     console.log('Connected: ' + frame);
+        //     this.stompClient.subscribe('/topic/greetings', function (greeting) {
+        //         showGreeting(JSON.parse(greeting.body).content);
+        //     });
+        // });
+
     }
 
-
-    handleData(data) {
-        let result = JSON.parse(data);
-        console.log(result)
+    handleSendName() {
+        this.stompClient.send("/app/hello", {}, JSON.stringify({ 'name': 'Hello' }))
     }
 
     render() {
@@ -43,6 +61,9 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <button onClick={this.handleClick.bind(this)}>Connect</button>
+                <button onClick={this.handleSendName.bind(this)}>Send Name</button>
+
             </div>
         );
     }
